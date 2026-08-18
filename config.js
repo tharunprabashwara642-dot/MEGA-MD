4a17f051a105ed9388076cf05c3540e71e0b6aa3
tharunprabashwara642-dot <tharunprabashwara642@gmail.com>
2026-08-18 21:48:45 +0530

    Update owner name and owner number to Tharun Prabhashwara and 94775585251

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const _prefixes = process.env.PREFIXES ? process.env.PREFIXES.split(',') : ['.',
 const config = {
     // Bot Identity
     botName: process.env.BOT_NAME || 'MEGA-MD',
-    botOwner: process.env.BOT_OWNER || 'Qasim Ali',
+    botOwner: process.env.BOT_OWNER || 'Tharun Prabhashwara',
     ownerNumber: process.env.OWNER_NUMBER || '94775585251',
-    author: process.env.AUTHOR || 'GlobalTechInfo',
+    author: process.env.AUTHOR || 'Tharun Prabhashwara',
     packname: process.env.PACKNAME || 'MEGA-MD',
     description: process.env.DESCRIPTION || 'High performance multi-device WhatsApp bot',
     version: '6.0.0',
@@ -13,14 +13,14 @@ const config = {
     prefixes: _prefixes,
     prefix: _prefixes[0],
     commandMode: process.env.COMMAND_MODE || 'public',
-    timeZone: process.env.TIMEZONE || 'Asia/Karachi',
+    timeZone: process.env.TIMEZONE || 'Asia/Colombo',
     // Links
     channelLink: process.env.CHANNEL_LINK || 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
     updateZipUrl: process.env.UPDATE_URL || 'https://github.com/GlobalTechInfo/MEGA-MD/archive/refs/heads/main.zip',
-    ytChannel: process.env.YT_CHANNEL || 'GlobalTechInfo',
+    ytChannel: process.env.YT_CHANNEL || 'Tharun Prabhashwara',
     // Session
     sessionId: process.env.SESSION_ID || '',
-    pairingNumber: process.env.PAIRING_NUMBER || '',
+    pairingNumber: process.env.PAIRING_NUMBER || '94775585251',
     // Performance
     port: Number(process.env.PORT) || 5000,
     maxStoreMessages: Number(process.env.MAX_STORE_MESSAGES) || 20,
@@ -49,7 +49,7 @@ const config = {
         'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
         'https://api.neoxr.my.id': process.env.NEOXR_KEY || 'yourkey',
         'https://violetics.pw': 'beta',
-        'https://zenzapis.xyz': process.env.ZENZAPIS_KEY || 'yourkey',
+        'https://zenzapis.xxz': process.env.ZENZAPIS_KEY || 'yourkey',
         'https://api-fgmods.ddns.net': 'fg-dylux'
     }
 };
